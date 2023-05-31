566dfdd0ac49ddaf5eae1df6bde80498abf923ef
cleberfc23 <cleberfc23@gmail.com>
2023-05-31 18:32:50 -0300

    refactorization - end of project

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -1,22 +1,32 @@
 package br.com.alura.agenda.database;
 
+import static br.com.alura.agenda.database.Migrations.ALL_MIGRATIONS;
+
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import br.com.alura.agenda.database.converter.ConversorCalendar;
 import br.com.alura.agenda.database.dao.RoomAlunoDAO;
 import br.com.alura.agenda.model.Aluno;
 
 @Database(
         entities = {Aluno.class},
-        version = 2,
+        version = 4,
         exportSchema = false)
 
+@TypeConverters({ConversorCalendar.class})
 public abstract class AgendaDatabase extends RoomDatabase {
 
     public static final String AGENDA_DB = "agenda.db";
+
+
     public static AgendaDatabase instance;
 
     public abstract RoomAlunoDAO getRoomAlunoDAO();
@@ -25,7 +35,7 @@ public abstract class AgendaDatabase extends RoomDatabase {
         if (instance == null) {
             instance = Room.databaseBuilder(context, AgendaDatabase.class, AGENDA_DB)
                     .allowMainThreadQueries()
-                    .fallbackToDestructiveMigration()
+                    .addMigrations(ALL_MIGRATIONS)
                     .build();
         }
         return instance;
--- app/src/main/java/br/com/alura/agenda/model/Aluno.java
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -6,6 +6,8 @@ import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @Entity
 public class Aluno implements Serializable {
@@ -13,29 +15,38 @@ public class Aluno implements Serializable {
     @PrimaryKey(autoGenerate = true)
     private int id = 0;
     private String nome;
-    private String sobrenome;
+    //    private String sobrenome;
     private String telefone;
     private String email;
+    private Calendar momentoDeCastastro = Calendar.getInstance();
 
-    @Ignore
-    public Aluno(String nome, String telefone, String email, String sobrenome) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.telefone = telefone;
-        this.email = email;
+//    @Ignore
+//    public Aluno(String nome, String telefone, String email, String sobrenome) {
+//        this.nome = nome;
+////        this.sobrenome = sobrenome;
+//        this.telefone = telefone;
+//        this.email = email;
+//    }
+//
+//    public Aluno() {
+//
+//    }
+
+    public Calendar getMomentoDeCastastro() {
+        return momentoDeCastastro;
     }
 
-    public Aluno() {
-
+    public void setMomentoDeCastastro(Calendar momentoDeCastastro) {
+        this.momentoDeCastastro = momentoDeCastastro;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
+//    public void setSobrenome(String sobrenome) {
+//        this.sobrenome = sobrenome;
+//    }
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
@@ -49,12 +60,12 @@ public class Aluno implements Serializable {
         return nome;
     }
 
-    public String getSobrenome() {
-        return sobrenome;
-    }
+//    public String getSobrenome() {
+//        return sobrenome;
+//    }
 
     public String getNomeCompleto() {
-        return (nome + " " + sobrenome);
+        return (getNome() + " ");
     }
 
     public String getTelefone() {
@@ -81,5 +92,10 @@ public class Aluno implements Serializable {
 
     public boolean temIdValido() {
         return id > 0;
+    }
+
+    public String getDataFormatada() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        return simpleDateFormat.format(this.momentoDeCastastro.getTime());
     }
 }
